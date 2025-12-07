6d9581f361145978a4e2448ce7ec0966dddb72a2
Brayan Barco <b.barco@nelumbo.com.co>
2025-12-07 13:11:28 -0500

    resolving problem N+1 in get users, with entitygraph (relation role, because of paginated)

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/repository/IUserRepository.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/repository/IUserRepository.java
@@ -1,6 +1,9 @@
 package com.pragma.powerup.infrastructure.out.jpa.repository;
 
 import com.pragma.powerup.infrastructure.out.jpa.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +12,6 @@ public interface IUserRepository extends JpaRepository<UserEntity, Long> {
     
     Optional<UserEntity> findByEmail(String email);
 
+    @EntityGraph(attributePaths = {"role"})
+    Page<UserEntity> findAll(Pageable pageable);
 }
